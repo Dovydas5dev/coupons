f99189999e2212c5559cdfdcba195fd5d27afe52
Dovydas <straycatgone@gmail.com>
2024-12-30 18:35:57 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,6 @@ lua54 'yes'
 author 'Dovydas5dev'
 version '1.0.0'
 
-shared_scripts {'@ox_lib/init.lua', '@es_extended/imports.lua'}
+shared_scripts {'@ox_lib/init.lua'}
 client_script 'client.lua'
 server_scripts {'server.lua', 'config.lua', '@oxmysql/lib/MySQL.lua'}
